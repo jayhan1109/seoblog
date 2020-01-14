b5a6464a368e5977618a1a22351f9d1ab1c5d61a
Jeongho Han <jungho1109@gmail.com>
2020-01-13 22:20:55 -0800

    Add initial layout, _document, bootstrap

---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+
+const Index = () => {
+  console.log("this is index page");
+
+  return (
+    <Layout>
+      <h2>Index Page</h2>
+    </Layout>
+  );
+};
+
+export default Index;
